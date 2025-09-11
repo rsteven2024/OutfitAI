224abcd3b444a25e527a052204d12aff756d9e2e
rsteven2024 <rstevenlc@gmail.com>
2025-09-11 13:58:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/outfitia/mainViewActivity.kt
@@ -0,0 +1,36 @@
+package com.example.outfitia
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class mainViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_view)
+
+        val btnContinuar = findViewById<Button>(R.id.btnContinuar)
+        btnContinuar.setOnClickListener {
+            navigateToPantallaPrincipalActivity()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    private fun navigateToPantallaPrincipalActivity() {
+        val intent = Intent(this, PantallaPrincipalActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+
