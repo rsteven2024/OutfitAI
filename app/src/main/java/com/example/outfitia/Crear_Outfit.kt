6376c3700cd5363769dd2c184c2c04cf478d0e04
rsteven2024 <rstevenlc@gmail.com>
2025-09-15 16:59:16 -0500

    Agregue la Camara funcional y Mostrar imagen tomada; arreglo del inicio de sesion

--- app/src/main/java/com/example/outfitia/Crear_Outfit.kt
+++ app/src/main/java/com/example/outfitia/Crear_Outfit.kt
@@ -1,20 +1,50 @@
 package com.example.outfitia
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Crear_Outfit : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_crear_outfit)
+
+        imageView = findViewById(R.id.imageView)
+        val btnCamara = findViewById<Button>(R.id.btnCamara)
+
+        btnCamara.setOnClickListener {
+            abrirCamara()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun abrirCamara() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            val extras = data.extras
+            val imageBitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
     }
 }
--- app/src/main/java/com/example/outfitia/mainViewActivity.kt
+++ app/src/main/java/com/example/outfitia/mainViewActivity.kt
@@ -3,6 +3,8 @@ package com.example.outfitia
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,14 +18,26 @@ class mainViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_view)
 
         val btnContinuar = findViewById<Button>(R.id.iniciarSesion)
-        btnContinuar.setOnClickListener {
-            pantallaPrincipalActivity()
-        }
+
         val btnCrearUsuario = findViewById<Button>(R.id.btncrearUsuario)
         btnCrearUsuario.setOnClickListener {
             crearUsuarioActivity()
         }
 
+
+        val usurio = findViewById<EditText>(R.id.usuarioCreado)
+        val contraseña = findViewById<EditText>(R.id.contraseñaCreada)
+
+        btnContinuar.setOnClickListener {
+            val contra1 = usurio.text.toString().trim()
+            val contra2 = contraseña.text.toString().trim()
+
+            if (contra1.isEmpty() || contra2.isEmpty()) {
+                Toast.makeText(this, "Ambos campos del Inicio de sesión son obligatorios", Toast.LENGTH_SHORT).show()
+            }else(
+                pantallaPrincipalActivity()
+            )
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
